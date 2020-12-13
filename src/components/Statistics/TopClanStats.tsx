b56a0760fecb22b5b0f9d9443d00c5bb43c2f674
Rostislav Klein <rostislav.klein@pipedrive.com>
2020-12-13 22:25:02 +0100

    top pvp stat mobile resolutio

--- src/components/Statistics/TopClanStats.tsx
+++ src/components/Statistics/TopClanStats.tsx
@@ -20,10 +20,26 @@ export const TopClanStats: React.FC<Props> = ({ stats, isLoading }) => {
     >
       <Table.Column render={(_, __, i) => `${i + 1}.`} width={20} />
       <Table.Column title="Clan Name" dataIndex="clan_name" />
-      <Table.Column title="Ally Name" dataIndex="ally_name" />
-      <Table.Column title="Clan Level" dataIndex="clan_level" />
-      <Table.Column title="Reputation Score" dataIndex="reputation_score" />
-      <Table.Column title="Clan Leader" dataIndex="leader_name" />
+      <Table.Column
+        title="Ally Name"
+        dataIndex="ally_name"
+        responsive={["lg", "sm"]}
+      />
+      <Table.Column
+        title="Clan Level"
+        dataIndex="clan_level"
+        responsive={["lg", "sm"]}
+      />
+      <Table.Column
+        title="Reputation Score"
+        dataIndex="reputation_score"
+        responsive={["lg", "sm"]}
+      />
+      <Table.Column
+        title="Clan Leader"
+        dataIndex="leader_name"
+        responsive={["lg", "sm"]}
+      />
       <Table.Column title="Members" dataIndex="ccount" />
       <Table.Column title="Total PvP" dataIndex="pvp_sum" />
     </StyledTable>
